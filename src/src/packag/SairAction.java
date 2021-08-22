b48833da86e90351b9cb53f015c111ff18d833ac
Liudimilla <42663858+Liudimilla@users.noreply.github.com>
2021-08-22 16:12:06 -0300

    Trabalhando com menus

---
+++ src/src/packag/SairAction.java
@@ -0,0 +1,12 @@
+package packag;
+import javax.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+
+public class SairAction extends AbstractAction {
+ public void actionPerformed(ActionEvent e) {
+	 System.exit(0);
+	 
+ }
+ 
+}
