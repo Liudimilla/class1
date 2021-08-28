45bff80cb01d48baf49ab0336011d07809e449dd
Liudimilla Silva <42663858+Liudimilla@users.noreply.github.com>
2021-08-28 18:12:14 -0300

    Trabalhando com Menus

---
+++ Menu/SairAction.java
@@ -0,0 +1,17 @@
+package Menu;
+
+
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+
+public class SairAction extends AbstractAction {
+ 
+
+@Override
+public void actionPerformed(java.awt.event.ActionEvent e) {
+	System.exit(0);
+	
+}
+ 
+}
