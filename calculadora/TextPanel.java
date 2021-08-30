ceb14867b3069bcbe85494e594be590e3fcfcb6d
Liudimilla Silva <42663858+Liudimilla@users.noreply.github.com>
2021-08-30 16:24:58 -0300

    Implementando uma calculadora usando Swing

--- calculadora/TextPanel.java
+++ calculadora/TextPanel.java
@@ -6,19 +6,19 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class TextPanel extends JPanel {
-	private JTextField txtNumber;
-	
+	private JTextField txtNumber;//criaçao de caixa de textp
+	// criar um painel (painel superior que ira receber os dados) JFrame
 	public TextPanel () {
 		setLayout(new FlowLayout());
 		
-		txtNumber = new JTextField(15);
-		txtNumber.setHorizontalAlignment(JTextField.RIGHT);
-		txtNumber.setEnabled(false);
+		txtNumber = new JTextField(15);// instaciar a caixa de texto
+		txtNumber.setHorizontalAlignment(JTextField.RIGHT);//alinha a caixa de texto
+		txtNumber.setEnabled(false);//p/ evitar que seja digita e somente receber o comando do teclado.
 		add(txtNumber);
 		
 		
 	}
-	public JTextField getTxtNumber() {
+	public JTextField getTxtNumber() { // para passar informaçoes para outra classe.
 		return txtNumber;
 	}
 
